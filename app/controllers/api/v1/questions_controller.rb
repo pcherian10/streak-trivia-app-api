199eaf4d634af6a3bf8e0e6394ea57aad8ea39f0
pcherian10 <pcherian10@gmail.com>
2019-01-21 17:22:19 -0500

    added relationships for user and questions

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,5 +1,26 @@
 class Api::V1::QuestionsController < ApplicationController
 
+  def index
+    @questions = Question.all
+    render json:@questions
+  end
+
+  def show
+    @question = Question(params[:id])
+    render json: @question
+  end
+
+
+  private
+
+  def question_params
+    params.require(:question).permit(:text, :first_choice, :second_choice,
+      :third_choice, :correct_answer )
+  end
+
+
+
+
 
 
 end
